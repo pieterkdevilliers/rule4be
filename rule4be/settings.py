251559bb5eb9a73aa0d92d9ac1ce335e0cef6ae1
Pieter K de Villiers <pieter@hey.com>
2023-09-06 15:20:59 +0100

    Update CORS

--- rule4be/settings.py
+++ rule4be/settings.py
@@ -164,6 +164,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://rule4-943c9feeb754.herokuapp.com",
 ]
 
 REST_FRAMEWORK = {
