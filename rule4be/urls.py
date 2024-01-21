58a077b992ec1d86dde07aabfc62e4ef73ff46f4
Pieter K de Villiers <pieter@hey.com>
2024-01-21 21:15:45 +0000

    HTMX for Login, View AOLS, View Today's Snapshots done

--- rule4be/urls.py
+++ rule4be/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('snapshots/', include('snapshots.urls')),
     path('api/v1/', include('snapshots.urls')),
-    path('login/', page_views.load_login_page, name='load_login_page'),
+    path('', page_views.load_login_page, name='load_login_page'),
+    path('aols/', page_views.load_aols_page, name='load_aols_page'),
+    path('today/<int:pk>', page_views.load_today_snapshot_page, name='load_today_snapshot_page'),
 ]
