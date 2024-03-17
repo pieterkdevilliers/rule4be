d0e46ebea0b9c85292ed4ca5ab06d0ae5e88427c
Pieter K de Villiers <pieter@hey.com>
2024-03-17 07:24:32 +0000

    Add CRUD elements throughout

---
+++ snapshots/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import AreaOfLife, Snapshot
+
+class AreaOfLifeForm(forms.ModelForm):
+    class Meta:
+        model = AreaOfLife
+        fields = ['name', 'description']
+
+class SnapshotForm(forms.ModelForm):
+    class Meta:
+        model = Snapshot
+        fields = ['body']
