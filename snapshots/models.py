495e56cab35dfa09bbfecfc7ea5098e605f15aef
Pieter K de Villiers <pieter@hey.com>
2024-08-01 16:56:18 +0100

    Add How It Works content

--- snapshots/models.py
+++ snapshots/models.py
@@ -2,19 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+
 class AreaOfLife(models.Model):
     owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     description = models.TextField()
+
     def __str__(self):
         return self.name
-    
+
+
 class Snapshot(models.Model):
     owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     area_of_life = models.ForeignKey('AreaOfLife', on_delete=models.CASCADE)
-    body = models.TextField()
-    image = models.ImageField(upload_to='snapshot-images/', null=True, blank=True)
-    video = models.FileField(upload_to='snapshot-videos/', null=True, blank=True)
+    body = models.TextField(max_length=1500, null=True, blank=True)
+    image = models.ImageField(
+        upload_to='snapshot-images/', null=True, blank=True)
+    video = models.FileField(
+        upload_to='snapshot-videos/', null=True, blank=True)
     created = models.DateField(auto_now_add=True)
+
     def __str__(self):
         return self.body
