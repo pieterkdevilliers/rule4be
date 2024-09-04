3614d0da5f3094b5c476d5a44f53569f09060611
Jean de Villiers <jeandevilliers@me.com>
2024-09-04 10:17:05 +0100

    Style remaining password reset pages

--- users/views.py
+++ users/views.py
@@ -20,6 +20,10 @@ class CustomPasswordResetDoneView(auth_views.PasswordResetDoneView):
     Custom Password Reset Done View
     """
     template_name = 'registration/password_reset_done.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['body_class'] = 'access-page password-reset-page'
+        return context
 
 
 class CustomPasswordResetConfirmView(auth_views.PasswordResetConfirmView):
@@ -28,6 +32,10 @@ class CustomPasswordResetConfirmView(auth_views.PasswordResetConfirmView):
     """
     template_name = 'registration/password_reset_confirm.html'
     success_url = reverse_lazy('password_reset_complete')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['body_class'] = 'access-page password-reset-page'
+        return context
 
 
 class CustomPasswordResetCompleteView(auth_views.PasswordResetCompleteView):
@@ -35,3 +43,7 @@ class CustomPasswordResetCompleteView(auth_views.PasswordResetCompleteView):
     Custom Password Reset Complete View
     """
     template_name = 'registration/password_reset_complete.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['body_class'] = 'access-page password-reset-page'
+        return context
