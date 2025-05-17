9a0f5e3b966215ced89ea8ee9fa36d91b4917ea0
pkdv <pieter.devilliers@reincubate.com>
2025-05-17 13:13:41 +0100

    bfa-25 move collectstatic to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@
 set -e
 
 # # Run Django collectstatic
-# echo "Collecting static files..."
-# python manage.py collectstatic --noinput # Assuming USE_S3=False is handled in settings.py
+echo "Collecting static files..."
+python manage.py collectstatic --noinput # Assuming USE_S3=False is handled in settings.py
 
 # Then exec the container's main process (what's specified as CMD)
 echo "Starting Gunicorn..."
